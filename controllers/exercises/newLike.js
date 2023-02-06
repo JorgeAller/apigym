450a668e8cdd38f82cdd553adb70d04abdf75e9f
JorgeAller <jorgeallertaboada@hotmail.com>
2023-02-06 22:11:55 +0100

    Fixed BBDD to LikeButton on frontend

--- controllers/exercises/newLike.js
+++ controllers/exercises/newLike.js
@@ -1,4 +1,5 @@
 const insertLikeQuery = require("../../bbdd/queries/exercises/insertLikeQuery");
+const selectExerciseByIdQuery = require("../../bbdd/queries/exercises/selectExerciseByIdQuery");
 
 const newLike = async (req, res, next) => {
   try {
@@ -8,15 +9,27 @@ const newLike = async (req, res, next) => {
     const msg = await insertLikeQuery(req.user.id, idExercise);
     console.log(msg);
 
+    const exercise = await selectExerciseByIdQuery(req.user.id, idExercise);
+    console.log(exercise);
     if (msg === "eliminado") {
       res.send({
         status: "ok",
         message: `Like eliminado`,
+        data: {
+          id: exercise.id,
+          likes: exercise.likes,
+          likedByMe: exercise.likedByMe,
+        },
       });
     } else {
       res.send({
         status: "ok",
         message: `Like agregado`,
+        data: {
+          id: exercise.id,
+          likes: exercise.likes,
+          likedByMe: exercise.likedByMe,
+        },
       });
     }
   } catch (err) {
